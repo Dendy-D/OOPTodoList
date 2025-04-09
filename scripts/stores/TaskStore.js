9ee884f6c85bbe9acd54f00507730a80e66748e5
Dendy-D <daniilvasilenko2@gmail.com>
2025-04-09 16:48:23 +0300

    feat: add Task store

--- scripts/stores/TaskStore.js
+++ scripts/stores/TaskStore.js
@@ -1,13 +1,49 @@
+import { LocalStorageService } from '../utils/LocalStorageService.js';
+
 class TaskStore {
-  #tasks = [];
+  #tasks;
+  #storage;
+
+  constructor() {
+    this.#storage = new LocalStorageService('task');
+    this.#tasks = this.#storage.getAllItems();
+  }
 
   get tasks() {
     return [...this.#tasks];
   }
 
-  getTaskById(id) {}
-  removeTask(id) {}
-  addTask() {}
+  getTasksOfFolder(folderId) {
+    return [...this.#tasks].filter((task) => task.folderId === folderId);
+  }
 
-  #saveTasks() {}
+  getTaskById(id) {
+    return this.#tasks.find((task) => task.id === id);
+  }
+
+  removeTask(id) {
+    this.#tasks = this.#tasks.filter((task) => task.id !== id);
+    this.#storage.removeItem(id);
+  }
+
+  removeAllFolderTasks(folderId) {
+    this.#tasks = this.#tasks.filter((task) => {
+      if (task.folderId === folderId) {
+        this.#storage.removeItem(task.id);
+        return;
+      }
+      return task;
+    });
+  }
+
+  addTask(task) {
+    this.#tasks.push(task);
+    this.#storage.setItem(task);
+  }
+
+  updateTasksFromStorage() {
+    this.#tasks = this.#storage.getAllItems();
+  }
 }
+
+export default new TaskStore();
